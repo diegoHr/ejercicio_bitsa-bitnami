3a91bf108fe651d69cb54e60e1adb1bb942b369e
Diego Hernando <diego.herev@gmail.com>
2021-06-09 15:14:27 +0200

    Added dependency injection library Hilt

---
+++ app/src/main/java/com/diegohr/ejercicio_bitsa_bitnami/CastleGameApp.kt
@@ -0,0 +1,10 @@
+package com.diegohr.ejercicio_bitsa_bitnami
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Diego Hernando on 9/6/21.
+ */
+@HiltAndroidApp
+class CastleGameApp : Application()
