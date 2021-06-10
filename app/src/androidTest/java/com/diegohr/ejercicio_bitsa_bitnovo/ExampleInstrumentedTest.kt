e801bbff896b64be61346f5aef165519585f7bea
Diego Hernando <diego.herev@gmail.com>
2021-06-10 14:08:41 +0200

    Fixed package name in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/diegohr/ejercicio_bitsa_bitnovo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/diegohr/ejercicio_bitsa_bitnovo/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.diegohr.ejercicio_bitsa_bitnami", appContext.packageName)
+        assertEquals("com.diegohr.ejercicio_bitsa_bitnovo", appContext.packageName)
     }
 }
